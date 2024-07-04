02e49d10eab0b0bf01d54be459cc7155f780f7e6
abhisathe <abhisathesteam@gmail.com>
2024-07-03 22:35:23 -0400

    array example

--- chapter_1/sandbox.js
+++ chapter_1/sandbox.js
@@ -5,3 +5,11 @@ let lastname = 'Sathe';
 let name = `Full name is ${firstname} ${lastname}`;
 
 console.log(name);
+
+
+
+//arrays
+
+let people = ['abhijit', 'sathe', 'anil']
+
+console.log(people[1]);
